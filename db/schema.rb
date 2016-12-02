4cf0c486f6c52143365c52c8e12eb4ab8201690c
Pranav <bean.pranav@gmail.com>
2016-12-02 00:07:09 -0500

    user reg column added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115233935) do
+ActiveRecord::Schema.define(version: 20161202050554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20161115233935) do
     t.boolean  "notification_friendship",   default: true
     t.boolean  "notification_conversation", default: true
     t.boolean  "notification_comment",      default: true
+    t.string   "reg_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
